6f287f61fc24b475b412330ff026792200936be4
jystella17 <jystella21617@gmail.com>
2021-03-04 00:57:54 +0900

    board update

--- models/models.go
+++ models/models.go
@@ -1,11 +1,24 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
 
 type User struct {
-	autoId      bson.ObjectId `json:"auto_id,omitempty" bson:"auto_id,omitempty"`
-	id          string        `json:"id" form:"id" binding:"required" bson:"id"`
-	password    string        `json:"password form:"password" binding:"required" bson:"password"`
-	name        string        `json:"name" form:"name" binding:"required" bson:"name"`
-	studentNum  int16         `json:"student_num" form:"student_num" binding:"required" bson:"student_num"`
+	autoId      bson.ObjectId  `json:"auto_id,omitempty" bson:"auto_id,omitempty"`
+	Id          string         `json:"id" form:"id" binding:"required" bson:"id"`
+	Password    string         `json:"password form:"password" binding:"required" bson:"password"`
+	Name        string         `json:"name" form:"name" binding:"required" bson:"name"`
+	StudentNum  int32          `json:"student_num" form:"student_num" binding:"required" bson:"student_num"`
+}
+
+type Board struct{
+	autoId     bson.ObjectId  `json:"auto_id,omitempty" bson:"auto_id,omitempty"`
+	Title      string         `json:"title" form:"title" binding:"required" bson:"title"`
+	Content    string         `json:"content" form:"content" bson:"content"`
+	Author     string         `json:"author" form:"author" binding"required" bson"author"`
+	AuthorNum  int32          `json:"author_num" form:"author_num" binding:"required" bson:"author_num"'`
+	Date       time.Time      `json:"date" form:"date" bson:"date"`
+	PageView   int64          `json:"page_view" form:"page_view" bson:"page_view"`
 }
