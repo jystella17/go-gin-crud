38eb3b08505186a299c95bdb59fc8f4de379e5ec
jystella17 <jystella21617@gmail.com>
2021-03-03 10:59:15 +0900

    user model

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func main(){
 	server := gin.Default()
+
 	server.GET("", func(c *gin.Context) {
 		c.String(200, "Hello, World")
 	})
--- models/models.go
+++ models/models.go
@@ -1,1 +1,11 @@
 package models
+
+import "gopkg.in/mgo.v2/bson"
+
+type User struct {
+	autoId      bson.ObjectId `json:"auto_id,omitempty" bson:"auto_id,omitempty"`
+	id          string        `json:"id" form:"id" binding:"required" bson:"id"`
+	password    string        `json:"password form:"password" binding:"required" bson:"password"`
+	name        string        `json:"name" form:"name" binding:"required" bson:"name"`
+	studentNum  int16         `json:"student_num" form:"student_num" binding:"required" bson:"student_num"`
+}
