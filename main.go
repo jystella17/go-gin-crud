035d0e61d1fafb76c8b8610f08cf549317793269
jystella17 <jystella21617@gmail.com>
2021-03-09 22:21:03 +0900

    router

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jystella17/go-gin-crud/service"
+	"log"
+	"time"
+)
 
 func main(){
-	server := gin.Default()
+	r := setupRouter()
 
-	server.GET("", func(c *gin.Context) {
-		c.String(200, "Hello, World")
-	})
+	if err := r.Run(); err != nil {
+		log.Fatalf("Failed to start Gin Engine")
+	}
 
-	server.Run(":8080")
+	r.Run()
+}
+
+func setupRouter() *gin.Engine{
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		AllowOrigins: []string{"http://localhost:8080"},
+		AllowMethods: []string{"GET","POST","PUT","DELETE","PATCH"},
+		AllowHeaders: []string{"Authorization","Access-Control-Allow-Origin","Origin","Content-Length","Content-Type"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 1 * time.Hour,
+	})) // CORS setting
+
+	router.GET("/home", service.GetHomePage)
+	router.GET("/user/:user_id", service.GetUserById)
+	router.GET("/board/list", service.GetAllPosts)
+	router.GET("/board/list/:user_id", service.GetPost)
+	router.GET("/board/create-post", service.CreatePost)
+	router.POST("/login", service.UserLogin)
+	router.POST("/register", service.UserRegister)
+	router.PUT("/board/update-post", service.UpdatePost)
+	router.DELETE("/board/delete-post", service.DeletePost)
+
+	return router
+
+	return router
 }
