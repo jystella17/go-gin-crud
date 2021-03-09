cba9790f406bd0a238f45be2805c2b1f245480cf
jystella17 <jystella21617@gmail.com>
2021-03-10 03:51:15 +0900

    mysql modeling

--- models/models.go
+++ models/models.go
@@ -2,27 +2,22 @@ package models
 
 import (
 	"time"
-	"gopkg.in/mgo.v2/bson"
 )
 
 type User struct {
-	Id          bson.ObjectId  `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserId      string         `json:"user_id,omitempty" form:"user_id" binding:"required" bson:"user_id,omitempty"`
-	Password    string         `json:"password form:"password" binding:"required" bson:"password"`
-	Name        string         `json:"name" form:"name" binding:"required" bson:"name"`
-	StudentNum  int32          `json:"student_num" form:"student_num" binding:"required" bson:"student_num"`
+	Id          int        `json:"id"`
+	UserId      string     `json:"userid"`
+	Password    string     `json:"password"`
+	Name        string     `json:"name"`
+	StudentNum  int        `json:"student_num"`
 }
 
 type Board struct{
-	Id         bson.ObjectId  `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title      string         `json:"title" form:"title" binding:"required" bson:"title"`
-	Content    string         `json:"content" form:"content" bson:"content"`
-	Author     string         `json:"author" form:"author" binding"required" bson"author"`
-	AuthorNum  int32          `json:"author_num" form:"author_num" binding:"required" bson:"author_num"'`
-	Date       time.Time      `json:"date" form:"date" bson:"date"`
-	PageView   int64          `json:"page_view" form:"page_view" bson:"page_view"`
-}
-
-func (s *Board) HandlePageView() {
-	s.PageView += 1
+	Id         int         `json:"id"`
+	Title      string      `json:"title"`
+	Content    string      `json:"content"`
+	Author     string      `json:"author"`
+	AuthorNum  int         `json:"author_num"'`
+	Date       time.Time   `json:"date"`
+	PageView   int         `json:"page_view"`
 }
