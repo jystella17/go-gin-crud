5a5a9bcf5412153ab1781a83f36237594ef8357e
jystella17 <jystella21617@gmail.com>
2021-03-11 12:48:12 +0900

    db

--- service/userapi.go
+++ service/userapi.go
@@ -1,28 +1,27 @@
 package service
 
 import (
-	//"github.com/jystella17/SM-Literature-Map/model"
 	"github.com/gin-gonic/gin"
-	//"go.mongodb.org/mongo-driver/mongo/options"
+	//"github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
-func GetHomePage(c *gin.Context){
+func (db *DB) GetHomePage(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"message" : "Go-Gin-CRUD",
 	})
 }
 
-func UserLogin(c *gin.Context){ // Login with POST Method
+func (db *DB) UserLogin(c *gin.Context){ // Login with POST Method
 }
 
-func UserRegister(c *gin.Context){ // Register with POST Method
+func (db *DB) UserRegister(c *gin.Context){ // Register with POST Method
 	return
 }
 
-func GetUserById(c *gin.Context){ // Load User information with GET Method
+func (db *DB) GetUserById(c *gin.Context){ // Load User information with GET Method
 	c.JSON(http.StatusOK, gin.H{"message" : "This is User Info Page"})
 }
 
-func GetPostById(c *gin.Context){
+func (db *DB) GetPostById(c *gin.Context){
 }
