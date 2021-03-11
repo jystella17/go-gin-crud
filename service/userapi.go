4b4223053475a9a228fc55ef8fc7f9c4db0c791e
jystella17 <jystella21617@gmail.com>
2021-03-11 13:01:46 +0900

    getallusers

--- service/userapi.go
+++ service/userapi.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jystella17/go-gin-crud/models"
+	"log"
+
 	//"github.com/go-sql-driver/mysql"
 	"net/http"
 )
@@ -10,6 +13,30 @@ func (db *DB) GetHomePage(c *gin.Context){
 	c.JSON(http.StatusOK, gin.H{
 		"message" : "Go-Gin-CRUD",
 	})
+}
+
+func (db *DB) GetAllUsers (c *gin.Context){
+	var (
+		user models.User
+		users []models.User
+	)
+
+	rows,err := db.Database.Query("SELECT auto_id, userid, password, name, student_num FROM user")
+	if err != nil {
+		log.Fatalf("SQL error : %s", err)
+		panic(err)
+	}
+
+	for rows.Next(){
+		err = rows.Scan(&user.Id, &user.UserId, &user.Password, &user.Name, &user.StudentNum)
+		users = append(users,user)
+		if err != nil{
+			log.Fatalf("SQL Scan Error : %s",err)
+			panic(err)
+		}
+	}
+	defer rows.Close()
+	c.JSON(http.StatusOK,users)
 }
 
 func (db *DB) UserLogin(c *gin.Context){ // Login with POST Method
